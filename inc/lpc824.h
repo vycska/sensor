1eb419910f5df5a43ed9b3f51489c697ae312f23
Vytas <vycska@gmail.com>
2018-04-06 14:57:34 +0300

    Papildytas lpc824.h

--- inc/lpc824.h
+++ inc/lpc824.h
@@ -3,6 +3,37 @@
 
 #include <stdint.h>
 
+// I/O configuration
+#define PIO0_17                        (*((volatile uint32_t*)0x40044000)) //I/O configuration for pin PIO0_17/ADC_9
+#define PIO0_13                        (*((volatile uint32_t*)0x40044004)) //I/O configuration for pin PIO0_17/ADC_9
+#define PIO0_12                        (*((volatile uint32_t*)0x40044008)) //I/O configuration for pin PIO0_17/ADC_9
+#define PIO0_5                         (*((volatile uint32_t*)0x4004400c)) //I/O configuration for pin PIO0_17/ADC_9
+#define PIO0_4                         (*((volatile uint32_t*)0x40044010)) //I/O configuration for pin PIO0_17/ADC_9
+#define PIO0_3                         (*((volatile uint32_t*)0x40044014)) //I/O configuration for pin PIO0_17/ADC_9
+#define PIO0_2                         (*((volatile uint32_t*)0x40044018)) //I/O configuration for pin PIO0_17/ADC_9
+#define PIO0_11                        (*((volatile uint32_t*)0x4004401c)) //I/O configuration for pin PIO0_17/ADC_9
+#define PIO0_10                        (*((volatile uint32_t*)0x40044020)) //I/O configuration for pin PIO0_17/ADC_9
+#define PIO0_16                        (*((volatile uint32_t*)0x40044024)) //I/O configuration for pin PIO0_17/ADC_9
+#define PIO0_15                        (*((volatile uint32_t*)0x40044028)) //I/O configuration for pin PIO0_17/ADC_9
+#define PIO0_1                         (*((volatile uint32_t*)0x4004402c)) //I/O configuration for pin PIO0_17/ADC_9
+#define PIO0_9                         (*((volatile uint32_t*)0x40044034)) //I/O configuration for pin PIO0_17/ADC_9
+#define PIO0_8                         (*((volatile uint32_t*)0x40044038)) //I/O configuration for pin PIO0_17/ADC_9
+#define PIO0_7                         (*((volatile uint32_t*)0x4004403c)) //I/O configuration for pin PIO0_17/ADC_9
+#define PIO0_6                         (*((volatile uint32_t*)0x40044040)) //I/O configuration for pin PIO0_17/ADC_9
+#define PIO0_0                         (*((volatile uint32_t*)0x40044044)) //I/O configuration for pin PIO0_17/ADC_9
+#define PIO0_14                        (*((volatile uint32_t*)0x40044048)) //I/O configuration for pin PIO0_17/ADC_9
+#define PIO0_28                        (*((volatile uint32_t*)0x40044050)) //I/O configuration for pin PIO0_17/ADC_9
+#define PIO0_27                        (*((volatile uint32_t*)0x40044054)) //I/O configuration for pin PIO0_17/ADC_9
+#define PIO0_26                        (*((volatile uint32_t*)0x40044058)) //I/O configuration for pin PIO0_17/ADC_9
+#define PIO0_25                        (*((volatile uint32_t*)0x4004405c)) //I/O configuration for pin PIO0_17/ADC_9
+#define PIO0_24                        (*((volatile uint32_t*)0x40044060)) //I/O configuration for pin PIO0_17/ADC_9
+#define PIO0_23                        (*((volatile uint32_t*)0x40044064)) //I/O configuration for pin PIO0_17/ADC_9
+#define PIO0_22                        (*((volatile uint32_t*)0x40044068)) //I/O configuration for pin PIO0_17/ADC_9
+#define PIO0_21                        (*((volatile uint32_t*)0x4004406c)) //I/O configuration for pin PIO0_17/ADC_9
+#define PIO0_20                        (*((volatile uint32_t*)0x40044070)) //I/O configuration for pin PIO0_17/ADC_9
+#define PIO0_19                        (*((volatile uint32_t*)0x40044074)) //I/O configuration for pin PIO0_17/ADC_9
+#define PIO0_18                        (*((volatile uint32_t*)0x40044078)) //I/O configuration for pin PIO0_17/ADC_9
+
 // system configuration
 #define SYSMEMREMAP                    (*((volatile uint32_t*)0x40048000)) //system memory remap
 #define PRESETCTRL                     (*((volatile uint32_t*)0x40048004)) //peripheral reset control
@@ -51,5 +82,35 @@
 #define PDAWAKECFG                     (*((volatile uint32_t*)0x40048234)) //power-down states for wake-up from deep-sleep
 #define PDRUNCFG                       (*((volatile uint32_t*)0x40048238)) //power configuration register
 #define DEVICE_ID                      (*((volatile uint32_t*)0x400483f8)) //device id
+
+// switch matrix
+#define PINASSIGN0                     (*((volatile uint32_t*)0x4000c000)) //pin assign register 0
+#define PINASSIGN1                     (*((volatile uint32_t*)0x4000c004)) //pin assign register 1
+#define PINASSIGN2                     (*((volatile uint32_t*)0x4000c008)) //pin assign register 2
+#define PINASSIGN3                     (*((volatile uint32_t*)0x4000c00c)) //pin assign register 3
+#define PINASSIGN4                     (*((volatile uint32_t*)0x4000c010)) //pin assign register 4
+#define PINASSIGN5                     (*((volatile uint32_t*)0x4000c014)) //pin assign register 5
+#define PINASSIGN6                     (*((volatile uint32_t*)0x4000c018)) //pin assign register 6
+#define PINASSIGN7                     (*((volatile uint32_t*)0x4000c01c)) //pin assign register 7
+#define PINASSIGN8                     (*((volatile uint32_t*)0x4000c020)) //pin assign register 8
+#define PINASSIGN9                     (*((volatile uint32_t*)0x4000c024)) //pin assign register 9
+#define PINASSIGN10                    (*((volatile uint32_t*)0x4000c028)) //pin assign register 10
+#define PINASSIGN11                    (*((volatile uint32_t*)0x4000c02c)) //pin assign register 11
+#define PINENABLE0                     (*((volatile uint32_t*)0x4000c1c0)) //pin enable register
+
+// NVIC
+#define ISER0                          (*((volatile uint32_t*)0xe000e100)) //interrupt set enable
+#define ICER0                          (*((volatile uint32_t*)0xe000e180)) //interrupt clear enable
+#define ISPR0                          (*((volatile uint32_t*)0xe000e200)) //interrupt set pending
+#define ICPR0                          (*((volatile uint32_t*)0xe000e280)) //interrupt clear pending
+#define IABR0                          (*((volatile uint32_t*)0xe000e300)) //interrupt active bit
+#define IPR0                           (*((volatile uint32_t*)0xe000e400)) //interrupt priority registers 0
+#define IPR1                           (*((volatile uint32_t*)0xe000e404)) //interrupt priority registers 1
+#define IPR2                           (*((volatile uint32_t*)0xe000e408)) //interrupt priority registers 2
+#define IPR3                           (*((volatile uint32_t*)0xe000e40c)) //interrupt priority registers 3
+#define IPR4                           (*((volatile uint32_t*)0xe000e410)) //interrupt priority registers 4
+#define IPR5                           (*((volatile uint32_t*)0xe000e414)) //interrupt priority registers 5
+#define IPR6                           (*((volatile uint32_t*)0xe000e418)) //interrupt priority registers 6
+#define IPR7                           (*((volatile uint32_t*)0xe000e41c)) //interrupt priority registers 7
 
 #endif
