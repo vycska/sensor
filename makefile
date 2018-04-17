1b77ac7f6a7c828734ef2141e2d9bfc7f38ec1b9
Vytas <vycska@gmail.com>
2018-04-17 20:35:32 +0300

    Pabandziau paleisti RTOS'a bet nepavyko; palikau siuos pakeitimus, t.y. PLL (veikia), pamazinti tasku stekai ir siaip eiluciu perstumdymas.

--- makefile
+++ makefile
@@ -75,8 +75,7 @@ cleanall : clean
 	rm -rf libs
 
 install : all
-	lpc21isp $(TARGET).hex /dev/ttyUSB0 115200 12000
-	#./lpc21isp ~/Programming/sensor/sensor.hex /dev/ttyUSB0 115200 12000
+	~/bin/lpc21isp $(TARGET).hex /dev/ttyUSB0 115200 12000
 
 picocom :
 	picocom -b 115200 --echo /dev/ttyUSB0
--- src/main.c
+++ src/main.c
@@ -14,26 +14,19 @@ volatile long long int millis;
 
 void main(void) {
    int i;
-   //PLL_Init();
+   PLL_Init();
    SYSAHBCLKCTRL |= (1<<1 | 1<<2 | 1<<3 | 1<<4 | 1<<6 | 1<<7 | 1<<10 | 1<<18); //enable clock for ROM, RAM0_1, FLASHREG, FLASH, GPIO, SWM, MRT, IOCON
    PRESETCTRL |= (1<<7 | 1<<10 | 1<<11); //clear MRT, GPIO, flash controller reset
 
-   //OS_Init(NUMTHREADS,"led",5,Task_Led,"idle",31,Task_Idle);
-
-   //OS_Start();
-   
-
-   //enable clock for switch matrix
-   //configure switch matrix
-   //disable any special function available for pint in PINENABLE0
    PINENABLE0 |= (1<<3 | 1<<16); //ACMP_I4 and ADC_3 disabled on pin PIO0_23
    PIO0_23 = 0<<3 | 0<<5 | 0<<6 | 0<<10 | 0<<11 | 0<<13; //no pu/pd, disable hysteresis, input not inverted, disable od, bypass input filter, peripheral clock divider 0
    DIR0 |= (1<<23); //set output direction
-   MASK0 &= (~(1<<23)); //enable reading and writing in MPORT registers
 
+   //OS_Init(NUMTHREADS,"led",5,Task_Led,"idle",31,Task_Idle);
+   //OS_Start();
 
    while(1) {
-      for(i=0;i<500000;i++);
+      for(i=0;i<15000000;i++);
       NOT0 = (1<<23);
    }
 }
@@ -50,13 +43,14 @@ void init(void) {
 }
 
 void Task_Led(void) {
-   int i;
-   for(i=0;i<1000000;i++);
-   OS_Sleep(2000);
+   while(1) {
+      NOT0 = (1<<23);
+      OS_Sleep(2000);
+   }
 }
 
 void Task_Idle(void) {
    while(1) {
-      WaitForInterrupt();
+      //WaitForInterrupt();
    }
 }
